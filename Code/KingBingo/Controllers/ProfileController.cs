24226811b86925bf7c32cb04f20b1577c91007a2
Joe Baldwin <contact@humboldttechgroup.com>
2014-03-13 23:29:53 -0500

    user profile from db on action parameter

--- Code/KingBingo/Controllers/ProfileController.cs
+++ Code/KingBingo/Controllers/ProfileController.cs
@@ -3,23 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KingBingo.DAL;
 
 namespace KingBingo.Controllers
 {
     public class ProfileController : Controller
     {
+        private KingBingoContext db = new KingBingoContext();
         //
         // GET: /Profile/
 
         public ActionResult Index(string username)
         {
             var Username = username;
-
-            ViewData["Username"] = username;
-
+            var user = db.UserProfiles.SingleOrDefault(u => u.UserName == username);
+            ViewData["User"] = user;
             return View();
-
-           
         }
 
     }
