42e7469ca55d61ae21efd12b34fc2f9b79d5242b
Joe Baldwin <contact@humboldttechgroup.com>
2014-03-14 15:01:23 -0500

    Fixed layout on views

---
+++ Code/KingBingo/Controllers/SecureController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KingBingo.Controllers
+{
+    public class SecureController : Controller
+    {
+        //
+        // GET: /Secure/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
