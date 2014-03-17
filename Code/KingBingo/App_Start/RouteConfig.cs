2692fbdb29169a5dc17dfdbf5cde662629d6b5b6
Joe Baldwin <contact@humboldttechgroup.com>
2014-03-17 15:20:01 -0500

    json view and service controller

--- Code/KingBingo/App_Start/RouteConfig.cs
+++ Code/KingBingo/App_Start/RouteConfig.cs
@@ -19,6 +19,11 @@ namespace KingBingo
                 defaults: new { controller = "Contact", action = "Index", id = UrlParameter.Optional }
             );
 
+            routes.MapRoute(
+               name: "Service",
+               url: "service/{action}",
+               defaults: new { controller = "Service", action = "Index", id = UrlParameter.Optional }
+           );
 
             routes.MapRoute(
               name: "Terms",
---
+++ Code/KingBingo/Controllers/ServiceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KingBingo.DAL;
+using KingBingo.Models;
+
+namespace KingBingo.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private KingBingoContext db = new KingBingoContext();
+        //
+        // GET: /Service/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult V0()
+        {
+
+            if (Request.QueryString["cmd"] != null)
+            {
+                var cmd = Request.QueryString["cmd"];
+                switch (cmd)
+                {
+                    case "allgames":
+                        ViewData["Games"] = db.Games;
+                        break;
+                }
+            }
+            return View();
+        }
+
+       
+
+    }
+}
