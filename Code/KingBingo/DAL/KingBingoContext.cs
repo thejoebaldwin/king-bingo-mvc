fc6afd6f2f431bac51d438fe7bc3ea46fcb6c60f
Joe Baldwin <contact@humboldttechgroup.com>
2014-03-18 23:54:16 -0500

    recreated solution

---
+++ Code/KingBingo/DAL/KingBingoContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using KingBingo.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Web.Security;
+using WebMatrix.WebData;
+
+namespace KingBingo.DAL
+{
+    public class KingBingoContext : DbContext
+    {
+
+        public KingBingoContext() : base("DefaultConnection")
+        {
+       
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<Badge> Badges { get; set; }
+        public DbSet<Friend> Friends { get; set; }
+      
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GameCard> GameCards { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Result> Results { get; set; }
+        public DbSet<Support> Supports { get; set; }
+        public DbSet<SystemMessage> SystemMessages { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+     
+    }
+}
---
+++ Code/KingBingo/Models/Friend.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KingBingo.Models
+{
+
+    public enum RequestStatus
+    {
+        Pending, Rejected, Accepted
+    }
+
+
+    public class Friend
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int FriendID { get; set; }
+        public DateTime Created { get; set; }
+
+        public virtual UserProfile User { get; set; }
+        public virtual UserProfile FriendUser { get; set; }
+        
+    }
+}
