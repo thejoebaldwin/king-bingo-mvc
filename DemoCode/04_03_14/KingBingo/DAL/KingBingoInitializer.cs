ee18f5babdbc6e3a4334b7e8b72c82fc16cdfcae
Joe Baldwin <contact@humboldttechgroup.com>
2014-04-03 10:30:03 -0500

    Initializers and Model generating

---
+++ DemoCode/04_03_14/KingBingo/DAL/KingBingoInitializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using KingBingo.Models;
+
+
+namespace KingBingo.DAL
+{
+    public class KingBingoInitializer
+    {
+        public void Seed(kingbingoEntities2 context)
+        {
+
+            //create a test user
+            string username = "test1";
+            string password = "test1";
+
+            var user = context.UserProfiles.SingleOrDefault(u => u.UserName == username);
+            if (user == null)
+            {
+                UserProfile.CreateUser(username, password);
+                user = context.UserProfiles.SingleOrDefault(u => u.UserName == username);
+                user.Bio = "This is my bio";
+                user.Birthdate = DateTime.Now.AddYears(-30);
+                context.SaveChanges();
+            }
+
+
+           
+
+
+
+        }
+
+
+    }
+}
---
+++ DemoCode/04_03_14/KingBingo/Models/KingBingo.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\baldwinj\Desktop\GitHub\king-bingo-mvc\DemoCode\04_03_14\KingBingo\Models\KingBingo.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
